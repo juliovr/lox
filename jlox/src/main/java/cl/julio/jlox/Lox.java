20951a83712ce410d080d15d829be59e821cfb7c
Julio Vidaurre <julio.vidaurre@gmail.com>
2024-11-12 18:15:16 -0300

    Chapter 4: Scanning

--- jlox/src/main/java/cl/julio/jlox/Lox.java
+++ jlox/src/main/java/cl/julio/jlox/Lox.java
@@ -6,10 +6,13 @@ import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class Lox {
 
-    public static void main(String[] args) {
+    private static boolean hadError = false;
+
+    public static void main(String[] args) throws IOException {
         if (args.length > 1) {
             System.out.println("Usage: jlox [script]");
             System.exit(64);
@@ -23,9 +26,11 @@ public class Lox {
     private static void runFile(String path) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
+
+        if (hadError) System.exit(65);
     }
 
-    private static void runPrompt() {
+    private static void runPrompt() throws IOException {
         InputStreamReader input = new InputStreamReader(System.in);
         BufferedReader reader = new BufferedReader(input);
 
@@ -35,7 +40,27 @@ public class Lox {
             if (line == null) break;
 
             run(line);
+
+            hadError = false;
         }
+    }
+
+    private static void run(String source) {
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+
+        for (Token token : tokens) {
+            System.out.println(token);
+        }
+    }
+
+    static void error(int line, String message) {
+        report(line, "", message);
+    }
+
+    private static void report(int line, String where, String message) {
+        System.err.printf("[line %d] Error %s: %s\n", line, where, message);
+        hadError = true;
     }
 
 }
