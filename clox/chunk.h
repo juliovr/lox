f677fcbda863bff74f047485bcb78e2f90b700a1
Julio Vidaurre <julio.vidaurre@gmail.com>
2024-12-06 16:47:01 -0300

    Chapter 14: Chunks of Bytecode

---
+++ clox/chunk.h
@@ -0,0 +1,25 @@
+#ifndef clox_chunk_h
+#define clox_chunk_h
+
+#include "common.h"
+#include "value.h"
+
+typedef enum {
+    OP_CONSTANT,
+    OP_RETURN,
+} OpCode;
+
+typedef struct {
+    int count;
+    int capacity;
+    u8 *code;
+    int *lines;
+    ValueArray constants;
+} Chunk;
+
+void init_chunk(Chunk *chunk);
+void free_chunk(Chunk *chunk);
+void write_chunk(Chunk *chunk, u8 byte, int line);
+int add_constant(Chunk *chunk, Value value);
+
+#endif
---
+++ clox/memory.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "memory.h"
+
+
+void *reallocate(void *pointer, size_t old_size, size_t new_size)
+{
+    if (new_size == 0) {
+        free(pointer);
+        return NULL;
+    }
+
+    void *result = realloc(pointer, new_size);
+    if (result == NULL) {
+        printf("Could not allocate memory. Terminating...");
+        exit(1);
+    }
+
+    return result;
+}
