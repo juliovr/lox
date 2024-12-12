bd81568b7cc0ea752d740d90e0abe838d168903f
Julio Vidaurre <julio.vidaurre@gmail.com>
2024-12-11 21:06:56 -0300

    Chapter 16: Scanning on Demand.

--- clox/vm.c
+++ clox/vm.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+
+#include "common.h"
+#include "compiler.h"
 #include "debug.h"
 #include "vm.h"
 
@@ -84,12 +87,11 @@ static InterpretResult run()
 #undef READ_BYTE
 }
 
-InterpretResult interpret(Chunk *chunk)
+InterpretResult interpret(const char *source)
 {
-    vm.chunk = chunk;
-    vm.ip = vm.chunk->code;
-
-    return run();
+    compile(source);
+    
+    return INTERPRET_OK;
 }
 
 void push(Value value)
